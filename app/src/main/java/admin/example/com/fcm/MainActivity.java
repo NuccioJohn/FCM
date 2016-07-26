8085c12c4a461d15754a2a4a144a7782b2ed49b4
nuccio <nuccio.john@gmail.com>
2016-07-26 11:23:29 -0400

    Added My FireBaseMessageService

--- app/src/main/java/admin/example/com/fcm/MainActivity.java
+++ app/src/main/java/admin/example/com/fcm/MainActivity.java
@@ -1,13 +1,23 @@
 package admin.example.com.fcm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.messaging.RemoteMessage;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        startService(new Intent(this, MyFirebaseMessagingService.class));
+        Intent intent = new Intent("admin.example.com.fcm.MyFirebaseMessagingService");
+        this.startService(intent);
+
     }
+
 }
